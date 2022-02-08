5a6a3fba872935429a16839c1c1cfeb75085f073
Aaryan hegde <hegde.aaryan@gmail.com>
2022-02-08 11:09:46 +0100

    Fireballs

---
+++ Vinter/Fireball.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Numerics;
+
+using Raylib_cs;
+
+
+public class Fireball
+{
+    public Vector2 direction = new Vector2(1, 0);
+    public Rectangle rect;
+    public Texture2D image = Raylib.LoadTexture("Fireball.png");
+
+    public Fireball()
+    {
+        rect = new Rectangle(50, 50, image.width, image.height);
+    }
+
+    public void Draw()
+    {
+        Raylib.DrawTexture(image, (int)rect.x, (int)rect.y, Color.WHITE);
+    }
+
+    public void Update()
+    {
+        rect.x += direction.X;
+        rect.y += direction.Y;
+    }
+
+}
--- Vinter/Program.cs
+++ Vinter/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using Raylib_cs;
 using System.Numerics;
 
 
-
 Raylib.InitWindow(1900, 1080, "The Game");
-
 
 Raylib.SetTargetFPS(60);
 
@@ -14,11 +13,28 @@ Texture2D Kratos = Raylib.LoadTexture("Kratos(EXsmall).png");
 Texture2D Demonking = Raylib.LoadTexture("DemonKing(SmallFlip).png");
 Texture2D hell = Raylib.LoadTexture("hell(1).png");
 Texture2D KratosATT = Raylib.LoadTexture("Kraosswing");
+Texture2D Fireball = Raylib.LoadTexture("Fireball");
+
+// Fireball f1 = new Fireball();
+
+List<Fireball> fireballs = new List<Fireball>();
+
+Fireball f1 = new Fireball();
+f1.rect.x = 50;
+f1.rect.y = 50;
+Fireball f2 = new Fireball();
+f1.rect.x = 50;
+f1.rect.y = 150;
+
+fireballs.Add(f1);
+fireballs.Add(f2);
+
 
 
 Rectangle Kratosrect = new Rectangle(100, 385, Kratos.width, Kratos.height);
 Rectangle DemonKingRect = new Rectangle(1600, 250, Demonking.width, Demonking.height);
 Rectangle KratosSwing = new Rectangle(100, 385, Kratos.width, Kratos.height);
+Rectangle Fireballrect = new Rectangle(20, 20, Fireball.width, Fireball.height);
 
 string level = "hell";
 float speed = 10;
@@ -26,8 +42,11 @@ float velocity = 0;
 float Gravity = 5;
 bool isGrounded = false;
 
+Vector2 Fireballdirection = new Vector2(1, 0);
+
 while (!Raylib.WindowShouldClose())
 {
+
 
     if (level == "hell" || level == "fairy_land" || level == "Abyss")
     {
@@ -37,6 +56,13 @@ while (!Raylib.WindowShouldClose())
         Kratosrect.y += movement.Y;
 
         velocity += Gravity;
+        if (movement.Length() > 0)
+        {
+            // Den här gör vectorn till 1 alltså normalizar
+            Fireballdirection = Vector2.Normalize(movement);
+        }
+
+
 
         if (Kratosrect.y >= 470 - Kratosrect.height)
         {
@@ -71,17 +97,22 @@ while (!Raylib.WindowShouldClose())
         Raylib.DrawTexture(KratosATT, 0, 0, Color.WHITE);
 
         Raylib.EndDrawing();
-
     }
 
 
+    // if (Raylib.IsKeyPressed(KeyboardKey.KEY_F))
+    // {
 
-
-
-
+    // }
 
     if (level == "hell")
     {
+
+        for (var i = 0; i < fireballs.Count; i++)
+        {
+            fireballs[i].Update();
+        }
+
 
         Raylib.BeginDrawing();
 
@@ -93,13 +124,16 @@ while (!Raylib.WindowShouldClose())
 
         Raylib.DrawTexture(Demonking, (int)DemonKingRect.x, (int)DemonKingRect.y, Color.WHITE);
 
+        for (int i = 0; i < fireballs.Count; i++)
+        {
+            fireballs[i].Draw();
+        }
+
         Raylib.EndDrawing();
 
     }
 
 }
-
-
 
 
 static Vector2 ReadMovement(float speed)
